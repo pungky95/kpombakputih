61d394e1ac285c9953a72f4fd0b49b388ad4b8c6
Pungky Kurniawan <pungkykurniawankr@gmail.com>
2017-04-24 22:15:30 +0700

    Galeri

---
+++ app/Galeri.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Galeri extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'galeris';
+
+    /**
+    * The database primary key value.
+    *
+    * @var string
+    */
+    protected $primaryKey = 'id';
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['fasilitas_id','kategori_id','blog_id','nama','mime','path','size'];
+
+    public function blog(){
+        return $this->belongsTo('App\Blog');
+    }
+    public function fasilita(){
+        return $this->belongsTo('App\Fasilita');
+    }
+    public function bungalow()
+    {
+        return $this->belongsToMany('App\Bungalow');
+    }
+    public function kategoris(){
+        return $this->belongsTo('App\Kategori');
+    }
+    
+}
